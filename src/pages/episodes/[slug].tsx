e6ca428a549d84102d57959dae3c83e8141d7d85
Italo Torres <italotorreslima2002@gmail.com>
2021-04-22 15:09:08 -0300

    fallback blocking

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -64,7 +64,7 @@ export default function Episode({ episode }: EpisodeProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
-    fallback: true,
+    fallback: "blocking",
   };
 };
 
